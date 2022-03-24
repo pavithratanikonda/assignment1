310ce52662e3572acbfb67a5c43bc091ff70e064
pavithratanikonda <97218666+pavithratanikonda@users.noreply.github.com>
2022-03-24 22:47:29 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -24,7 +24,7 @@ public class BubbleSortExample {
                 }  
                 System.out.println();  
                   
-                bubbleSort(arr);//sorting array elements using bubble sort  
+                bubbleSort(arr);
                  
                 System.out.println("Array After Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
